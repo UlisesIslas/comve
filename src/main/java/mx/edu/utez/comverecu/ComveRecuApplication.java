c3169940cf6ddb4490d6891dd3f83b19129ba1ea
unknown <20193tn013@utez.edu.mx>
2022-04-22 20:56:57 -0500

    Inicio proyecto

---
+++ src/main/java/mx/edu/utez/comverecu/ComveRecuApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.comverecu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComveRecuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComveRecuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/utez/comverecu/ComveRecuApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.comverecu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComveRecuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
