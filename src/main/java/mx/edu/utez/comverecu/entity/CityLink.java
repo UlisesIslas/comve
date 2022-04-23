40260809d630ebf0894fc3de1f206395679433ed
unknown <20193tn013@utez.edu.mx>
2022-04-22 22:03:49 -0500

    Entitys de la bd

---
+++ src/main/java/mx/edu/utez/comverecu/entity/CityLink.java
@@ -0,0 +1,10 @@
+package mx.edu.utez.comverecu.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "city_link")
+public class CityLink {
+    
+}
---
+++ src/main/java/mx/edu/utez/comverecu/entity/Suburb.java
@@ -0,0 +1,10 @@
+package mx.edu.utez.comverecu.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "suburb")
+public class Suburb {
+    
+}
