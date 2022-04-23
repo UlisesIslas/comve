1a32ff8beea50d912e2e339eb32e86a30c730bdf
unknown <20193tn013@utez.edu.mx>
2022-04-23 05:13:52 -0500

    Módulo de Gestión de Usuarios (Enlaces)

--- src/main/java/mx/edu/utez/comverecu/controller/UserController.java
+++ src/main/java/mx/edu/utez/comverecu/controller/UserController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import mx.edu.utez.comverecu.entity.CityLink;
 import mx.edu.utez.comverecu.entity.Users;
+import mx.edu.utez.comverecu.entity.DataTransferObject.RecoverPasswordDto;
 import mx.edu.utez.comverecu.entity.DataTransferObject.UserDto;
 import mx.edu.utez.comverecu.security.BlacklistController;
 import mx.edu.utez.comverecu.service.CityLinkService;
@@ -45,22 +46,27 @@ public class UserController {
     private CityService cityService;
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String findAll(Model model, Pageable pageable) {
+    public String findAll(Model model, Pageable pageable, Authentication authentication, HttpSession session) {
+        Users user = userService.findByUsername(authentication.getName());
+        user.setPassword(null);
+        session.setAttribute("user", user);
         Page<Users> listUsers = userService
-        .listPagination(PageRequest.of(pageable.getPageNumber(), 10, Sort.by("id").descending()));
+                .listPagination(PageRequest.of(pageable.getPageNumber(), 10, Sort.by("id").ascending()));
         model.addAttribute("listUsers", listUsers);
         return "users/list";
     }
 
-    @RequestMapping(value = "/find/{id}", method = RequestMethod.GET)
-    public String findOne(Model model, @PathVariable("id") long id, RedirectAttributes redirectAttributes) {
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String findOne(Model model, @PathVariable("id") long id, RedirectAttributes redirectAttributes,
+            RecoverPasswordDto recoverPasswordDto) {
         Users user = userService.findById(id);
         if (!user.equals(null)) {
             model.addAttribute("user", user);
+            return "/users/edit";
         } else {
             redirectAttributes.addFlashAttribute("msg_error", "No se encontró el usuario solicitado");
         }
-        return "";
+        return "redirect:/users/list";
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.GET)
@@ -69,9 +75,48 @@ public class UserController {
         return "users/create";
     }
 
+    @RequestMapping(value = "/recover/{id}", method = RequestMethod.POST)
+    public String recoverPassword(RecoverPasswordDto recoverPasswordDto, Model model,
+            RedirectAttributes redirectAttributes, @PathVariable("id") long id,
+            Authentication authentication, HttpSession session) {
+        if (!BlacklistController.checkBlacklistedWords(recoverPasswordDto.getPassword())) {
+            boolean res;
+            Users user = userService.findByUsername(authentication.getName());
+            user.setPassword(null);
+            session.setAttribute("user", user);
+            user.setPassword(userService.findPasswordById(id));
+            Users tmpUser = userService.findById(id);
+            tmpUser.setPassword(passwordEncoder.encode(recoverPasswordDto.getPassword()));
+            if(user.getUsername().equals(tmpUser.getUsername())) {
+                res = userService.save(tmpUser);
+                redirectAttributes.addFlashAttribute("msg_success", "Se modificó tu contraseña correctamente, vuelve a iniciar sesión");
+                if (res) {
+                    return "redirect:/logout";
+                } else {
+                    redirectAttributes.addFlashAttribute("msg_error", "Ocurrió un problema al modificar la contraseña");
+                }
+            } else {
+                res = userService.save(tmpUser);
+            }
+            if (res) {
+                redirectAttributes.addFlashAttribute("msg_success", "Se modificó la contraseña correctamente");
+                return "redirect:/users/list";
+            } else {
+                redirectAttributes.addFlashAttribute("msg_error", "Ocurrió un problema al modificar la contraseña");
+            }
+        } else {
+            redirectAttributes.addFlashAttribute("msg_error", "Ingresó una o más palabras prohibidas");
+        }
+        return ("redirect:/users/edit/" + id);
+    }
+
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
     public String signup(UserDto userDto, Model model, RedirectAttributes redirectAttributes) {
-        if (!(BlacklistController.checkBlacklistedWords(userDto.getName()) || BlacklistController.checkBlacklistedWords(userDto.getLastname()) || BlacklistController.checkBlacklistedWords(userDto.getUsername()) || BlacklistController.checkBlacklistedWords(userDto.getPhone()) || BlacklistController.checkBlacklistedWords(userDto.getPassword()))) {
+        if (!(BlacklistController.checkBlacklistedWords(userDto.getName())
+                || BlacklistController.checkBlacklistedWords(userDto.getLastname())
+                || BlacklistController.checkBlacklistedWords(userDto.getUsername())
+                || BlacklistController.checkBlacklistedWords(userDto.getPhone())
+                || BlacklistController.checkBlacklistedWords(userDto.getPassword()))) {
             if (!linkService.hasCityLink(userDto.getCity())) {
                 Users obj = new Users();
                 obj.setName(userDto.getName());
@@ -82,15 +127,16 @@ public class UserController {
                 obj.setEmail(userDto.getEmail());
                 obj.setPassword(passwordEncoder.encode(userDto.getPassword()));
                 obj.addRole(rolesService.findByAuthority("ROL_ENLACE"));
-                
+
                 boolean res = userService.save(obj);
 
                 CityLink tmpLink = new CityLink();
                 tmpLink.setCity(cityService.findOne(userDto.getCity()));
                 tmpLink.setUser(obj);
-                boolean res2 = linkService.save(tmpLink); 
+                boolean res2 = linkService.save(tmpLink);
                 if (res && res2) {
-                    redirectAttributes.addFlashAttribute("msg_success", "Enlace registrado correctamente, Ahora puede iniciar sesión con este usuario");
+                    redirectAttributes.addFlashAttribute("msg_success",
+                            "Enlace registrado correctamente, Ahora puede iniciar sesión con este usuario");
                     return "redirect:/users/list";
                 } else {
                     redirectAttributes.addFlashAttribute("msg_error", "Ocurrió un error al registrar al Enlace");
@@ -99,15 +145,15 @@ public class UserController {
                 redirectAttributes.addFlashAttribute("msg_error", "El municipio ya tiene un enlace asignado");
             }
         } else {
-            redirectAttributes.addFlashAttribute("msg_error", "Ungresó una o más palabras prohibidas");
+            redirectAttributes.addFlashAttribute("msg_error", "Ingresó una o más palabras prohibidas");
         }
-        
+
         return "redirect:/users/create";
     }
 
     @RequestMapping(value = "/disable/{id}", method = RequestMethod.GET)
     public String disableUser(@PathVariable("id") long id, RedirectAttributes redirectAttributes,
-    Authentication authentication, HttpSession session) {
+            Authentication authentication, HttpSession session) {
         Users user = userService.findByUsername(authentication.getName());
         user.setPassword(null);
         session.setAttribute("user", user);
@@ -129,5 +175,5 @@ public class UserController {
         userService.save(tmp);
         return "redirect:/users/list";
     }
-    
+
 }
---
+++ src/main/java/mx/edu/utez/comverecu/entity/DataTransferObject/RecoverPasswordDto.java
@@ -0,0 +1,14 @@
+package mx.edu.utez.comverecu.entity.DataTransferObject;
+
+public class RecoverPasswordDto {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
